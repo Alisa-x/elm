15fd87e3e6c4bde599474b5157af5a1eb0a843c2
furunhuang <392539758@qq.com>
2018-07-26 15:35:35 +0800

    726中午修改

--- src/components/details/menu.vue
+++ src/components/details/menu.vue
@@ -2,16 +2,16 @@
     <div class="menu">
         <div class="left">
             <div ref="left">
-                <div class="left_main">
+                <div class="left_main" ref="leftMain">
                     <ul>
-                        <li v-for="(commodity,index) in data.commodity">{{commodity.title}}</li>
+                        <li v-for="(commodity,index) in data.commodity" :class="{flag:flag===index}" @click="clickLi(index)">{{commodity.title}}</li>
                     </ul>
                 </div>
             </div>
         </div>
         <div class="right" ref="right">
-            <div class="right_main">
-                <dl v-for="(commodity,index) in data.commodity">
+            <div class="right_main" ref="rightMain">
+                <dl v-for="(commodity,index) in data.commodity" ref="oDl" >
                     <dt>{{commodity.title}}</dt>
                     <dd v-for="(commoditys,item) in commodity.commoditys">
                         <img :src="commoditys.img" alt="">
@@ -56,7 +56,9 @@
         data(){
             return {
                 data:null,
-                distance:0
+                distance:0,
+                flag:0,
+                timer:null,
             }
         },
         created(){
@@ -68,12 +70,56 @@
         },
         updated(){
             this.$refs.right.onscroll=()=>{
-                console.log(1);
-                this.$refs.right.scrollTop > this.distance &&
-                (document.body.scrollTop=document.documentElement.scrollTop = this.$refs.right.scrollTop );
-                this.distance = this.$refs.right.scrollTop;
+                let scrollbar = this.$refs.right.scrollTop;
+
+                let leftLength = this.$refs.leftMain.clientHeight-this.$refs.left.clientHeight;
+                let rightLength = this.$refs.rightMain.clientHeight-this.$refs.right.clientHeight;
+                let rightPct = scrollbar/(this.$refs.rightMain.clientHeight-this.$refs.right.clientHeight);
+                this.$refs.left.scrollTop = leftLength*rightPct;
+                //左右滚动条绑定
+                if(scrollbar > this.distance){
+                    document.body.scrollTop= document.body.scrollTop+(scrollbar-this.distance);
+                    document.documentElement.scrollTop = document.documentElement.scrollTop +(scrollbar-this.distance);
+                }
+                this.distance = scrollbar;
+                //主滚动条和右副滚动条绑定
+                [...this.$refs.oDl].some((item,index,ary)=>{
+                    let top = item.offsetTop-1;
+                    let bot = item.offsetTop + item.clientHeight-1;
+                    if(top<=scrollbar&&scrollbar<bot){
+                        rightLength < scrollbar+10 ? this.flag=ary.length-1 : this.flag = index;
+                        return true
+                    }
+
+                });
+                //右滚动条左边类名绑定
             }
-        }
+        },
+        methods:{
+            clickLi(index){
+                this.animated(index,500)
+            },
+            animated(index,time1){
+                let time2 = 0;
+                let scrollTop1 = this.$refs.oDl[index].offsetTop-this.$refs.right.scrollTop;
+                let scrollTop2 = this.$refs.right.scrollTop;
+                clearInterval(this.timer);
+                 this.timer = setInterval(()=>{
+                    time2+=17;
+                    if(time2>time1){
+                        clearInterval(this.timer);
+                        this.$refs.right.scrollTop = this.$refs.oDl[index].offsetTop;
+                    }else{
+                        this.$refs.right.scrollTop = scrollTop2+scrollTop1*(time2/time1);
+                    }
+
+                },17)
+            },
+        },
+        computed:{
+        },
+        watch:{
+        },
 
     }
 </script>
@@ -102,6 +148,9 @@
                         width: 2.2rem;
                         line-height: 1.4rem;
                     }
+                    li.flag{
+                        background: #fff;
+                    }
                 }
             }
         }
@@ -110,6 +159,7 @@
             overflow-y: scroll;
             .right_main{
                 padding-bottom: .5rem;
+                position: relative;
                 dl{
                     dt{
                         font-size: .4rem;
